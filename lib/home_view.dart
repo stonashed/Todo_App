20e7c7e769547a0a4aa84fb527c6ed08ff52d4bf
PatienceA26 <afiapatienceb@gmail.com>
2021-11-30 16:30:49 +0000

    styled completed

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/utiles.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -6,39 +7,40 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          leading: const Padding(
-            padding: EdgeInsets.only(left: 15),
-            child:
-                CircleAvatar(backgroundImage: AssetImage('assets/africa.jpg')),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: const Padding(
+          padding: EdgeInsets.only(left: 15),
+          
+          child: CircleAvatar(backgroundImage: AssetImage('assets/africa.jpg')
           ),
-          title: const Text(
-            'My Task',
-          ),
-          elevation: 0,
-          actions: [
-            Icon(Icons.sort),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Icon(Icons.search),
-            ),
-          ],
         ),
-        body: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-          child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(children: [
-                Icon(
-                  Icons.check_circle_outline,
-                  color: Colors.pink,
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Column(
+        title: const Text(
+          'My Task',
+        ),
+        elevation: 0,
+        actions: [
+          Icon(Icons.sort),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(children: [
+              Icon(
+                Icons.check_circle_outline,
+                color: Colors.pink,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -52,50 +54,69 @@ class HomeView extends StatelessWidget {
                         height: 5,
                       ),
                       Text(
-                        'this is my first trip to germany on a plane,if it is your first time please come to the back',
-                        maxLines: 2,
+                        'this is my first trip to germany on a plane, if it is your first time please come to the back',
+                        maxLines: 3,
                         overflow: TextOverflow.ellipsis,
                         style: TextStyle(color: Colors.grey, fontSize: 10),
                       ),
                     ]),
-                const SizedBox(
-                  width: 10,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.notifications,
-                      color: Colors.pink,
-                    ),
-                    Text('Yesterday', style: TextStyle(color: Colors.pink)),
-                  ],
-                )
-              ])),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.notifications,
+                    color: Colors.pink,
+                  ),
+                  Text('Yesterday', style: TextStyle(color: Colors.pink)),
+                ],
+              )
+            ])),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          size: 30,
         ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(
-            Icons.add,
-            size: 30,
-          ),
-          onPressed: () {},
-          backgroundColor: Colors.blue,
-        ),
-        bottomNavigationBar: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-            child: Material(
+        onPressed: () {},
+        backgroundColor: Colors.blue,
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          child: Material(
               borderRadius: BorderRadius.circular(10),
               color: Color.fromRGBO(37, 43, 103, 0.4),
               child: Padding(
                 padding: const EdgeInsets.all(15.0),
                 child: Row(
                   children: [
-                    Icon(Icons.check_circle_rounded),
+                    Icon(Icons.check_circle_rounded, color: customBlue),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Row(
+                      children: [
+                        Text('Completed',
+                            style: TextStyle(
+                                color: customBlue,
+                                fontWeight: FontWeight.w600)),
+                        Icon(Icons.keyboard_arrow_down, color: customBlue),
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    ),
+                    const Spacer(),
+                    Text(
+                      '24',
+                      style: TextStyle(color: customBlue),
+                    )
                   ],
                 ),
-              ),
-            ),
-          ),
-        ));
+              )),
+        ),
+      ),
+    );
   }
 }
