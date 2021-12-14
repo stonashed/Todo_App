ad2e3dbce0127ce0116e08838399053d399f87fb
PatienceA26 <afiapatienceb@gmail.com>
2021-12-14 16:35:13 +0000

    no changes today

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -11,6 +11,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         backgroundColor: Colors.white,
         leading: const Padding(
