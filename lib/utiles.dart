9817f7f1fb794f8d6147df52156714afd05e99d7
PatienceA26 <afiapatienceb@gmail.com>
2021-12-14 16:32:34 +0000

    color was define there

--- lib/utiles.dart
+++ lib/utiles.dart
@@ -2,3 +2,13 @@ import 'package:flutter/material.dart';
 
 Color customBlue = Color.fromRGBO(37, 43, 103, 1);
 
+Color dateColor(String date) {
+  switch (date) {
+    case 'yesterday':
+      return Colors.pinkAccent;
+    case 'today':
+      return Colors.blueAccent;
+    default:
+      return customBlue;
+  }
+}
